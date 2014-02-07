eb738cd1393346f7cb5debd9091a8985d1100700
Alex Bennée <alex@bennee.com>
2014-02-07 11:35:23 +0000

    Build up basic module structure

---
+++ heatingpi/__init__.py
@@ -0,0 +1,6 @@
+#
+# __init__.py
+#
+"""
+Heating Pi is a selection of modules to allow you to build a simple temperature control system
+"""
---
+++ heatingpi/sensor/__init__.py
@@ -0,0 +1,11 @@
+#
+#
+#
+"""
+The sensor modules are ones that get values from various sensors.
+These can be anything from a little microcontroller controlled
+temperatur sensor to something that polls a remote weather station or
+even something as fancy as checking the local network for phones as a
+measure of occupancy.
+"""
+

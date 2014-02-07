8872d056c2298bab863ae1ef305bdcd284cce4f1
Alex Bennée <alex@bennee.com>
2014-02-07 12:30:32 +0000

    heatingpi: flesh out module documentation a bit

--- heatingpi/__init__.py
+++ heatingpi/__init__.py
@@ -1,6 +1,18 @@
+# Heating Pi, A set of modules to build a temperature control system
 #
-# __init__.py
+# Copyright (C) 2014 Alex Bennée <alex@bennee.com>
+#
+# Author: Alex Bennée <alex@bennee.com>
+# URL: http://github.com/stsquad/heating-pi.git
+#
+# Licensed under the GPLv3 or later, see LICENSE in top directory
 #
 """
-Heating Pi is a selection of modules to allow you to build a simple temperature control system
+Heating Pi is a selection of modules to allow you to build a simple
+temperature control system. The idea is you supply a basic function to
+decide the target temperature and a set of sensors and control modules
+and the rest will take care of itself.
 """
+
+__author__ = "Alex Bennée"
+__license__ = "GPL"
